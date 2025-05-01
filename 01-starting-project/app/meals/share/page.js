e3c32b56fcd97e9fb17bc2e7f6659b6654470721
dkekzhs <68364199+dkekzhs@users.noreply.github.com>
2025-05-01 23:27:10 +0900

    chore : Essential 4 Completed all parts

--- 01-starting-project/app/meals/share/page.js
+++ 01-starting-project/app/meals/share/page.js
@@ -3,9 +3,10 @@ import { formMeal } from '@/lib/actions';
 import ImagePicker from '../image-picker';
 import classes from './page.module.css';
 import FormSumbit from './meal-form-sumbit';
+import { useFormState } from 'react-dom';
 
 export default function ShareMealPage() {
-
+  const [state, formAction] = useFormState(formMeal, {message : null});
   return (
     <>
       <header className={classes.header}>
@@ -15,7 +16,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form} action={formMeal}>
+        <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -44,6 +45,7 @@ export default function ShareMealPage() {
             ></textarea>
           </p>
           <ImagePicker name="image" />
+          <p>{ state.message && <p>{state.message}</p>}</p>
           <p className={classes.actions}>
             <FormSumbit/>
           </p>
--- 01-starting-project/lib/actions.js
+++ 01-starting-project/lib/actions.js
@@ -1,9 +1,13 @@
 'use server'
 
-import { redirect } from "next/dist/server/api-utils";
+import { redirect } from "next/navigation";
 import { saveMeal } from "./meals"
 
-export async function formMeal( form ) {
+function invalidText(text) {
+    return !text || text.trim() === '';
+}
+
+export async function formMeal( prevState, form ) {
     const meal = {
       title: form.get('title'),
       summary: form.get('summary'),
@@ -12,6 +16,17 @@ export async function formMeal( form ) {
       creator: form.get('name'),
       creator_email: form.get('email'),
     }
+
+    if (invalidText(meal.title) ||
+        invalidText(meal.summary) ||
+        invalidText(meal.instructions) ||
+        invalidText(meal.creator) ||
+        invalidText(meal.creator_email) ||
+        !meal.creator_email.includes("@")
+    ) {
+        return { "message": "invaild Input" };
+    }
+
     await saveMeal(meal);
     redirect("/meals");
   }
